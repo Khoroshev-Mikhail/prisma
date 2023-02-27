a3414e7646409fb5c66991a4ed01c1d6e7751a1a
Mikha38 <oyks1379@yandex.ru>
2023-02-27 19:51:43 +0300

    test

--- components/ui/СontractRow.tsx
+++ components/ui/СontractRow.tsx
@@ -12,56 +12,56 @@ export default function ContractRow({...props}:incomingProps){
     const {data: session} = useSession()
     const {trigger} = useSWRMutation(`/api/contracts/${props.id}`, updateApi)
 
-    // async function handlerAccepted(val: boolean | null){
-    //     const formData = new FormData()
-    //     formData.append('id', String(props.id))
-    //     formData.append('email', String(session.user.email))
-    //     if(val === true){
-    //         formData.append('accepted', '')
-    //         await trigger(formData)
-    //         await props.mutate()
-    //     }
-    //     if(!val){
-    //         formData.append('accepted', 'true')
-    //         await trigger(formData)
-    //         await props.mutate()
-    //     }
-    // }
-    // async function handlerRejected(val: boolean | null){
-    //     const formData = new FormData()
-    //     formData.append('id', String(props.id))
-    //     formData.append('email', String(session.user.email))
-    //     if(val === false){
-    //         formData.append('accepted', '')
-    //         await trigger(formData)
-    //         await props.mutate()
-    //     }
-    //     if(val === true || val === null){
-    //         formData.append('accepted', 'false')
-    //         await trigger(formData)
-    //         await props.mutate()
-    //     }
-    // }
+    async function handlerAccepted(val: boolean | null){
+        const formData = new FormData()
+        formData.append('id', String(props.id))
+        formData.append('email', String(session.user.email))
+        if(val === true){
+            formData.append('accepted', '')
+            await trigger(formData)
+            await props.mutate()
+        }
+        if(!val){
+            formData.append('accepted', 'true')
+            await trigger(formData)
+            await props.mutate()
+        }
+    }
+    async function handlerRejected(val: boolean | null){
+        const formData = new FormData()
+        formData.append('id', String(props.id))
+        formData.append('email', String(session.user.email))
+        if(val === false){
+            formData.append('accepted', '')
+            await trigger(formData)
+            await props.mutate()
+        }
+        if(val === true || val === null){
+            formData.append('accepted', 'false')
+            await trigger(formData)
+            await props.mutate()
+        }
+    }
     return (
         <div className="py-2 grid grid-cols-12 border-t border-gray-200">
-        <div className="p-2 col-span-2 border-r border-gray-200">{props.name}</div>
+        {/* <div className="p-2 col-span-2 border-r border-gray-200">{props.name}</div>
         <div className="p-2 col-span-2 border-r border-gray-200">{new Date(props.date).toLocaleDateString()}</div>
         <div className="p-2 col-span-2 border-r border-gray-200">{props.partner.form} {props.partner.name}</div>
         <div className="p-2 col-span-2 border-r border-gray-200 flex justify-center">
         <Button.Group>
-            {/* <Button onClick={()=>handlerAccepted(props.accepted)} color={props.accepted === true ? 'success' : 'gray'} className="w-1/2">
+            <Button onClick={()=>handlerAccepted(props.accepted)} color={props.accepted === true ? 'success' : 'gray'} className="w-1/2">
                 {props.accepted === true ? 'Принят' : 'Принять'}
             </Button>
             <Button onClick={()=>handlerRejected(props.accepted)} color={props.accepted === false ? 'failure' : 'gray'} className="w-1/2">
                 {props.accepted === false ? 'Отклонён' : 'Отклонить'}
-            </Button> */}
+            </Button>
         </Button.Group>
         </div>
         <div className="p-2 col-span-1 border-r border-gray-200 text-center">Скачать</div>
         <div className="p-2 col-span-2 border-r border-gray-200">{props.description}</div>
         <div className="p-2 col-span-1 text-center">
             <Link href={`/contracts/edit/${props.id}`} className="font-medium text-blue-600 hover:underline dark:text-blue-500">Edit</Link>
-        </div>
+        </div> */}
       </div>
       )
 }
--- pages/contracts/index.tsx
+++ pages/contracts/index.tsx
@@ -77,7 +77,8 @@ export default function Contracts({fallbackData}:{fallbackData: ContractExt[]}){
         </div>
         {data && sorted.map((el, i) => {
             return (
-                <ContractRow {...el} mutate={mutate} key={i}/>
+              <></>
+                // <ContractRow {...el} mutate={mutate} key={i}/>
             )
       })} 
     </Layout>
