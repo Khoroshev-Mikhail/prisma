a4245f455586f205c23e4fbd3b1ef8ba8c8acac2
Mikha38 <oyks1379@yandex.ru>
2023-02-27 20:43:19 +0300

    fix errors

--- components/ui/СontractRow.tsx
+++ components/ui/СontractRow.tsx
@@ -17,7 +17,7 @@ export default function ContractRow({...props}:incomingProps){
         formData.append('id', String(props.id))
         formData.append('email', String(session.user.email))
         if(val === true){
-            formData.append('accepted', '')
+            formData.append('accepted', 'null')
             await trigger(formData)
             await props.mutate()
         }
