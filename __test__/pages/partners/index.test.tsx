c36d5a8dc4cc00ee72e6161081286b7a116a6faf
Mikha38 <oyks1379@yandex.ru>
2023-03-06 22:13:28 +0300

    add some tests

--- __test__/pages/partners/index.test.tsx
+++ __test__/pages/partners/index.test.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "../../test-utils";
 import "@testing-library/jest-dom"
-// import PartnerPage from "@/pages/partners";
+import PartnerPage from "@/pages/partners";
+import "@testing-library/jest-dom";
+import {useSession} from "next-auth/react";
+// import { cache } from "swr/_internal";
+jest.mock("next-auth/react");
 
-
-// describe('first test', () => {
-//     it('should render properly', () => {
-
-
-
-//     })
-// })
+describe('first test', () => {
+    afterEach(() => {
+        // cache.clear()
+      });
+    it('should render properly', () => {
+        (useSession as jest.Mock).mockReturnValue({user: {email: '79836993884@ya.ru', id: 1, accessLevel: 4}})
+        render(<PartnerPage />)
+        const header = screen.getByText(/ara/i)
+        const headerText = 'ara'
+        expect(header).toHaveTextContent(headerText)
+    })
+})
