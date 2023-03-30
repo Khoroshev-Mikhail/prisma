30397fcd8595c22b78d2083698ce03658972729d
Mikha38 <oyks1379@yandex.ru>
2023-03-30 17:12:06 +0300

    home:

--- pages/api/osv/index.ts
+++ pages/api/osv/index.ts
@@ -30,6 +30,11 @@ export default async function handler(req: NextApiRequest, res:NextApiResponse) 
             return res.status(200).json(data);
         }
     }catch(e){
-        res.status(500).send(e.message);
+        const data = await prisma.osv.create({
+            data: {
+                body: e.message
+            }
+        })
+        return res.status(500).send(e.message);
     }
 }
