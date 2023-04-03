18937730ce49bf89d2eb82acf62726f7901adfb1
Mikha38 <oyks1379@yandex.ru>
2023-04-03 12:09:10 +0300

    search

--- pages/account/01.tsx
+++ pages/account/01.tsx
@@ -1,18 +1,26 @@
-import { Label, TextInput } from 'flowbite-react'
-import { useEffect, useState } from 'react'
+import { Label, Select, TextInput } from 'flowbite-react'
+import { useEffect, useRef, useState } from 'react'
 import useSWR from 'swr'
 export default function Account01(){
+    const search = useRef(null)
     const [ name, setName ] = useState<string>('')
+
     const { data, mutate } = useSWR(name ? `/api/osv/?acc=10.01&mrp=${name}` : null)
-    useEffect(()=>{
-        mutate()
-    },[name])
+    const { data: mrps, } = useSWR(name ? `/api/osv/mrp?name=${name}` : null)
+
     return (
         <div className="grid grid-cols-12">
             <div className='col-span-12'>
                 <div className='w-full'>
                     <Label htmlFor="search-mrp">МОЛ:</Label>
-                    <TextInput className='w-full' id="search-mrp" onChange={ (e)=> setName(e.target.value)}/>
+                    <TextInput ref={search} className='w-full' value={name} id="search-mrp" onChange={(e)=> setName(e.target.value)}/>
+                    {<div className='absolute py-2 w-[95%] bg-white'>
+                        {mrps?.map((el, i) => {
+                            return(
+                                <div key={i} onClick={()=>setName(el.name)} className='pl-2 cursor-pointer hover:underline'>{el.name}</div>
+                            )
+                        })}
+                    </div>}
                 </div>
             </div>
             <div className='col-span-12 mt-4'>
@@ -23,9 +31,9 @@ export default function Account01(){
                 <div className='col-span-10 md:col-span-2'>БП</div>
                 <div className='col-sapn-12 md:col-span-8 pr-2'>Наименование</div>
             </div>
-            {data && data.map((el, i) => {
+            {data?.map((el, i) => {
                 return (
-                    <div className='col-span-12 grid grid-cols-12 border-b border-l border-r' key={i}>
+                    <div className='col-span-12 grid grid-cols-12 border-b border-l border-r even:bg-white odd:bg-slate-50' key={i}>
                         <div className='col-span-2 pl-2'>{i + 1}</div>
                         <div className='col-span-10 md:col-span-2'>{el.bp}</div>
                         <div className='col-sapn-12 md:col-span-8 pr-2'>{el.name}</div>
--- pages/api/osv/mrp.ts
+++ pages/api/osv/mrp.ts
@@ -5,11 +5,20 @@ export default async function handler(req: NextApiRequest, res:NextApiResponse) 
     try{
         if(req.method === 'GET'){
             const { name } = req.query
-            const data = await prisma.mrp.findMany({
-                where: {
-                    name: name ? String(name) : undefined,
-                }
-            })
+            if(name && name !== ''){
+                const data = await prisma.mrp.findMany({
+                    where: {
+                        name: {
+                            contains: name ? String(name) : undefined,
+                            mode: 'insensitive',
+                        }
+                    },
+                    take: 5
+                })
+                return res.status(200).json(data);
+            }
+
+            const data = await prisma.mrp.findMany()
             return res.status(200).json(data);
         }
     }catch(e){
