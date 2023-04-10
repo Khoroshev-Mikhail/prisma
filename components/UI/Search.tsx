e74456307c7f893b3cad6e0eda0c17c626702030
Mikha38 <oyks1379@yandex.ru>
2023-04-10 15:39:00 +0300

    sd

--- components/UI/Search.tsx
+++ components/UI/Search.tsx
@@ -24,15 +24,13 @@ export default function Search({ data, name, setName, isLoading }){
             <TextInput className='w-full' value={name} id="search-mrp" onChange={(e)=>setSearch(e.target.value)} autoComplete='off'/>
             {visible && 
                 <div className='absolute w-[97.7%] bg-white py-2 border rounded-md'>
-                    {isLoading && <Spinner color={'pink'} />}
-                    {!isLoading && data &&
-                        data.map((el, i) => {
-                            return(
-                                <div key={i} onClick={()=>setName(el.name)} className='ml-2 cursor-pointer hover:underline hover:bg-slate-50'>
-                                    {el.name}
-                                </div>
-                            )
-                        })
+                    {data?.map((el, i) => {
+                        return(
+                            <div key={i} onClick={()=>setName(el.name)} className='ml-2 cursor-pointer hover:underline hover:bg-slate-50'>
+                                {el.name}
+                            </div>
+                        )
+                    })
                 }
                 </div>
             }
