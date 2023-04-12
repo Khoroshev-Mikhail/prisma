9f5333345830a76a85b9559d742c0a48a712adbd
Mikha38 <oyks1379@yandex.ru>
2023-04-12 13:20:33 +0300

    tel

--- components/UI/Search.tsx
+++ components/UI/Search.tsx
@@ -14,7 +14,9 @@ export default function Search({ data, state, setState,  placeholder, isLoading,
         }
     }
     useEffect(()=>{
-        ref.current.focus()
+        if(autofocus){
+            ref.current.focus()
+        }
         addEventListener('click', clickSearch)
         return ()=>{
             removeEventListener('click', clickSearch)
@@ -23,7 +25,7 @@ export default function Search({ data, state, setState,  placeholder, isLoading,
     return (
         <div className='w-full'>
             {/* <Label htmlFor="search-mrp">{label}</Label> */}
-            <TextInput ref={ref} type="number" inputMode="numeric" className='w-full bg-white' value={state} id="search-mrp" onChange={setSearch} autoFocus autoComplete='off' placeholder={placeholder}/>
+            <input ref={ref} type="tel" inputMode="numeric" className='w-full bg-white rounded-lg border-gray-300' value={state} id="search-mrp" onChange={setSearch} autoComplete='off' placeholder={placeholder}/>
             {visible && 
                 <div className='absolute w-[92%] bg-white py-2 border rounded-md'>
                     {isLoading && <Spinner className="ml-2" />}
