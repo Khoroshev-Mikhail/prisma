d14ecef5d14ede21230272ea32ee606ecad81a1d
Mikha38 <oyks1379@yandex.ru>
2023-04-12 11:15:41 +0300

    fix

--- pages/osv/stock.tsx
+++ pages/osv/stock.tsx
@@ -6,7 +6,7 @@ import Head from 'next/head'
 import { useEffect, useRef, useState } from 'react'
 import useSWR from 'swr'
 export default function Stock(){
-    const [ name, setName ] = useState<string>('')
+    const [ name, setName ] = useState<string>(null)
 
     const { data, isLoading } = useSWR(name ? `/api/osv/?stock=${name}` : null)
     const { data: list } = useSWR(`/api/osv/stocks`)
@@ -20,7 +20,7 @@ export default function Stock(){
             <div className="grid grid-cols-12">
                 <div className='col-span-12'>
                 {list && 
-                    <select className='w-full rounded-lg border-gray-300' defaultValue={null} onChange={(e)=>setName(e.target.value)}>
+                    <select className='w-full rounded-lg border-gray-300' value={name} onChange={(e)=>setName(e.target.value)}>
                         {!list.includes("") && <option value={null}></option>}
                         {list.map((el, i) => {
                             return (
@@ -36,7 +36,7 @@ export default function Stock(){
                         <Spinner size={'lg'} />
                     </div>
                 }
-                {!isLoading && data && <MCTable data={data.filter(el => lowCostAccounts.includes(el.acc))} title={'Малоценные средства'} />}
+                {!isLoading && data && <MCTable data={data} title={'Малоценные средства'} />}
             </div>
         </>
     )
