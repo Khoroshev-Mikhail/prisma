7a29583375fbc0f40ab84c7ac46b9803cf364f4f
Mikha38 <oyks1379@yandex.ru>
2023-04-12 11:55:55 +0300

    ara

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
-import useSWR from 'swr'
 export default function Index(){
-
-    const { data, isLoading } = useSWR(`/api/osv/`)
-    useEffect(()=>{
-        if(data){
-            const ara = data.map(el => el.acc)
-            const set = new Set(ara)
-        }
-    }, [data])
     return (
         <div className="grid grid-cols-12">
         </div>
